7ae19f70bd9e26dbb3d85d35362ad1e769d1e693
andre <andre.jacques.pujol@gmail.com>
2023-02-15 10:08:15 -0600

    first 2 sections finished

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "./components/index";
+// const Main = () => {
+//   return (
+//     <div id="main">
+//       <div id="navbar">
+//         <div>Contact List</div>
+//       </div>
+//       <div id="container">{/* your components here */}</div>
+//     </div>
+//   );
+// };
 
-const Main = () => {
-  return (
-    <div id="main">
-      <div id="navbar">
-        <div>Contact List</div>
-      </div>
-      <div id="container">
-        {/* your components here */}
-      </div>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <Main />,
-  document.getElementById('app')
-);
+ReactDOM.render(<Main />, document.getElementById("app"));
